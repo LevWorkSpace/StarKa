7422fb55a0de141c67515fc14da0dff8771b0639
levworkspace <levworkspace@gmail.com>
2015-10-01 23:47:24 +0300

    refactoring

--- src/SomeGraphs/Bullet.java
+++ src/SomeGraphs/Bullet.java
@@ -1,6 +1,7 @@
 package SomeGraphs;
 
 import java.awt.Image;
+import java.util.LinkedList;
 
 
 
@@ -8,14 +9,15 @@ public class Bullet extends Object{
 	public Bullet(int x, int y, Image[] img,Image[] imgDeath,int v, int health,boolean enemy) {
 		super("Bullet",x, y, v,0,health, enemy, img, imgDeath);
 	}
-	void action(Space s){
-		for (int i=0;i<s.objectBase.size();i++){
-			Object obj=s.objectBase.get(i);
+	public int action(LinkedList<Object> objectBase){
+		for (int i=0;i<objectBase.size();i++){
+			Object obj=objectBase.get(i);
 			if (obj.invize==false&&!obj.name.equals("Bullet")&&this.testContact(obj)&&this.enemy!=obj.enemy&&health>0){
 				obj.damaged(health);
 				die();
 			}
 		}
+		return 0;
 	}
 }
 
